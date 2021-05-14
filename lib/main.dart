99fbac8748314947a667ab682342007d591d39a2
yahya <yahya.ensas@hotmail.com>
2021-05-14 15:55:00 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(child: Image.asset('images/dice1.png'),onPressed: (){
+              print('Left button get pressed');
+            },),),
+          Expanded(child: FlatButton(child: Image.asset('images/dice2.png'),onPressed: (){
+            print('Right button get pressed');
+          },),)
+        ],
+      ),
+    );
+  }
+}
